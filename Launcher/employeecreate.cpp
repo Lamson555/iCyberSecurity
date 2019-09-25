c0b8bbf4e29a017a0948f23d41624e22876359da
LittleJimmyFirl <50810262+LittleJimmyFirl@users.noreply.github.com>
2019-09-25 15:07:44 -0700

    Add files via upload

--- Launcher/employeecreate.cpp
+++ Launcher/employeecreate.cpp
@@ -1,6 +1,10 @@
 #include "employeecreate.h"
 #include "ui_employeecreate.h"
 #include "mainwindow.h"
+#include "accountconfirm.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
 
 EmployeeCreate::EmployeeCreate(QWidget *parent) :
     QDialog(parent),
@@ -19,4 +23,46 @@ void EmployeeCreate::on_pushButton_clicked()
     hide();
     MainWindow *mainWindow = new MainWindow();
     mainWindow->show();
+}
+
+void EmployeeCreate::on_pushConfirm_clicked()
+{
+    QFile ownerFile("/Users/Hamad/CS1C_Project_One/employee.txt");
+
+    if(!ownerFile.open(QFile::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this,"Error","Database File is Not Open");
+    }
+    QTextStream out(&ownerFile);
+    QString firstName = ui->firstText->toPlainText();
+    QString lastName = ui->lastText->toPlainText();
+    QString email = ui->emailText->toPlainText();
+    QString phoneNumber = ui->phoneText->toPlainText();
+    QString userID = ui->userText->toPlainText();
+    QString password = ui->passText->toPlainText();
+    QString passwordCon = ui->passConText->toPlainText();
+
+    out << firstName + '\n';
+    out << lastName + '\n';
+    out << email + '\n';
+    out << phoneNumber + '\n';
+    out << userID + '\n';
+    out << password + '\n';
+    out << passwordCon + '\n' + '\n';
+
+    ownerFile.flush();
+    ownerFile.close();
+
+    this->ui->firstText->setText("");
+    this->ui->lastText->setText("");
+    this->ui->emailText->setText("");
+    this->ui->phoneText->setText("");
+    this->ui->userText->setText("");
+    this->ui->passText->setText("");
+    this->ui->passConText->setText("");
+
+    accountConfirm account;
+    account.setModal(true);
+    account.exec();
+    hide();
 }
